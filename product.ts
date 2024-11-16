2d2accc2592ea3581737c7a71a2b2bd1fadc9cc9
ibrahim hamshari <Ibraheem.Hamshari@gmail.com>
2024-11-16 13:53:55 +0200

    done the fifth task

--- product.ts
+++ product.ts
@@ -7,3 +7,9 @@ interface Product {
 function calculateTotalPrice(products: Product[]): number {
   return products.reduce((total, product) => total + product.price, 0);
 }
+
+// Task 5
+function isValidEmail(email: string): boolean {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
