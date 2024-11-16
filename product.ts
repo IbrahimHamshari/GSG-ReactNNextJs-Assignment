b7fcf1842d2dad21c858b373136c980dfa6f5a86
ibrahim hamshari <Ibraheem.Hamshari@gmail.com>
2024-11-16 14:04:34 +0200

    add the brief explanation as requested

--- product.ts
+++ product.ts
@@ -13,3 +13,13 @@ function isValidEmail(email: string): boolean {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return emailRegex.test(email);
 }
+
+/*
+  Typescript works by checking the types at the compile time, and then compile it to js code to be able to run it.
+
+  To compile it to js, we use :
+  tsc product.ts
+
+  then we can run it using:
+  node product.js
+*/
